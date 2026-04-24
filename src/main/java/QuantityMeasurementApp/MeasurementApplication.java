b482fa3e94759d093a4a378d9febc78912d67649
abhay7004mca25-cloud <abhay7009.ca22@chitkara.edu.in>
2026-04-24 09:30:37 +0530

    feature/UC3-genericlength

--- src/main/java/QuantityMeasurementApp/MeasurementApplication.java
+++ src/main/java/QuantityMeasurementApp/MeasurementApplication.java
@@ -1,36 +1,19 @@
 package QuantityMeasurementApp;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.quantity.measurement.enums.LengthUnit;
+import com.quantity.measurement.model.QuantityLength;
 
-//@SpringBootApplication
 public class MeasurementApplication {
 
-    static class Feet {
-        private final double value; // immutable
-
-        public Feet(double value) {
-            this.value = value;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) {
-                return true;
-            }
-            if (obj == null || getClass() != obj.getClass()) {
-                return false;
-            }
-            Feet other = (Feet) obj;
-            return Double.compare(this.value, other.value) == 0;
-        }
-    }
-
-
     public static void main(String[] args) {
-        SpringApplication.run(MeasurementApplication.class, args);
-        Feet f1 = new Feet(34.5);
-        Feet f2 = new Feet(34.5);
-        System.out.println(f1.equals(f2));
+
+        QuantityLength q1 = new QuantityLength(1.0, LengthUnit.FEET);
+        QuantityLength q2 = new QuantityLength(12.0, LengthUnit.INCH);
+
+        if (q1.equals(q2)) {
+            System.out.println("Equal (true)");
+        } else {
+            System.out.println("Not Equal (false)");
+        }
     }
 }
